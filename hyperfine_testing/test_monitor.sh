73475f8f0d347e636265fb07264b8a3cc5f9eec2
William Stone <william_l_stone@brown.edu>
2025-06-24 23:53:50 -0400

    Redid command execution through lower level fork + exec, altered regex matching to go through line by line streaming, added files for hyperfine testing, and refactored some things/cleaned things up

---
+++ hyperfine_testing/test_monitor.sh
@@ -0,0 +1,1 @@
+../target/release/stream-monitor -- $1
--- src/main.rs
+++ src/main.rs
@@ -1,132 +1,128 @@
-use regex;
+use regex::Regex;
 use clap::Parser;
-use std::process::Command;
-// use pprof::ProfilerGuard;
-// use std::fs::File;
+//Low level shell command utilities
+use nix::unistd::{pipe, close, ForkResult, fork, execvp, Pid};
+use nix::sys::wait::waitpid;
+use nix::libc;
+//Standard library helpers
+use std::ffi::CString;
+use std::io::{ BufRead, BufReader, Read };
+use std::os::fd::AsRawFd;
+use std::fs::File;
+use std::process::exit;
 
 mod timer;
 use crate::timer::Timer;
 
+#[cfg(test)]
+mod tests;
+
 //OUTSTANDING TASKS/QUESTIONS
 //Test performance and speed up!
-//  -  Make performance test file
-//  -  Replace regex library with quicker ripgrep regex engine/custom engine?
-//Ensure compliance with the entire range of stream types out there (account for any divergence with typical regex)
+//  -  Make performance test file - DONE (more or less)
+//  -  Replace regex library with quicker ripgrep regex engine/custom engine? Update: regex-automata is solution!
+//Ensure compliance with the entire range of stream types out there (account for any divergence with typical regex) - DONE: there is no divergence
 //  -  Are speedups acheivable if regex matching takes into account any limitations in the expressivity of the stream
-//     types w.r.t. standard regex?
-//  -  Are stream types always fully specific (ie. could always be written as '^<stream_type>$')?
+//     types w.r.t. standard regex? 
+//  -  Are stream types always fully specific (ie. could always be written as '^<stream_type>$')? - YES
 //Create Makefile to streamline binary creation/testing - DONE
 //Create README with explanation and usage instructions
 //Create more/better tests as additions continue
-//Add command line options?
+//Figure out how input validation should work
 //Create external process execution monitor to halt entire script's execution upon monitor failure? 
 //Figure out how to handle errors that crop up from running input commands themselves
 //Monitor output from each individual member of a long piped command for more precise debugging?
+//Pipe validated output to stdout as received to maintain the benefits of a stream-based approach (instead of println!)
 
 #[derive(Parser, Debug)]
 #[command(version, about, long_about = None)]
 struct Args {
-    #[arg()]
-    reg_exp: String,
+    #[arg(short, required(false))]
+    input_regex: Option<String>,
+
+    #[arg(short, required(false))]
+    output_regex: Option<String>,
 
     #[arg()]
     command_parts: Vec<String>
 }
 
 fn main() {
-    //let guard = ProfilerGuard::new(100).unwrap();
     let _total_time = Timer::new("Total");
-
+    //Alert user of potential unintented functionality if no input/output types are specified
     let args = Args::parse();
-    let command = args.command_parts.join(" ");
-    let output = monitor(command, args.reg_exp);
-    println!("{:?}", output);
-
-    // if let Ok(report) = guard.report().build() {
-    //     let mut file = File::create("flamegraph.svg").unwrap();
-    //     report.flamegraph(&mut file).unwrap();
-    //     println!("Flamegraph written to flamegraph.svg");
-    // }
+    match (&args.input_regex, &args.output_regex) { //I wish I could do this with if let but alas...
+        (Option::None, Option::None) => println!("Warning: no type specified for input or output of command, so no validation will take place"),
+        _ => {}
+    }
+    let validated_output = monitor(args.command_parts, args.input_regex, args.output_regex);
+    println!("{}", validated_output);
 }
 
-fn monitor(command: String, pattern: String) -> String {
-    //Run command and retrieve output to stdout
-    let command_timer = Timer::new("Shell Command");
-    let command_out = Command::new("sh")
-        .arg("-c").arg(&command)
-        .output()
-        .expect("Command no run :(");
-    let stdout = String::from_utf8_lossy(&command_out.stdout);
-    drop(command_timer);
-    //Act according to whether output matches expected pattern
-    let _regex_timer = Timer::new("Regex Match");
-    let output_match = regex::Regex::new(&pattern).expect("Provided regular expression invalid");
-    if output_match.is_match(&stdout) { stdout.to_string() }
-    else { panic!("Command output did not match pattern.\nPattern: {:?}\nCommand output: {:?}", pattern, stdout) }
+/// Given a shell command (represented as a vector of Strings), ensures that the input to that command matches any
+/// specified regex ipattern (coming soon!) and that the output to that command matches any specified regex opattern,
+/// returning the command's output if both validations are satisfied and panicking if not.
+fn monitor(command: Vec<String>, _ipattern: Option<String>, opattern: Option<String>) -> String {
+    let _monitor_timer = Timer::new("Full Monitor");
+    //Run command and retrieve output stream
+    let _command_timer = Timer::new("Run Command");
+    let (mut buffed_stream, child_pid) = run_command(command);
+    drop(_command_timer);
+    //Validate output
+    let _validation_timer = Timer::new("Output Validation");
+    let output = if let Option::Some(opattern) = opattern { validate_stream(buffed_stream, opattern) } 
+    else { 
+        let mut output_buf = String::new();
+        buffed_stream.read_to_string(&mut output_buf).expect("Reading output buffer to a String failed");
+        output_buf
+    };
+    drop(_validation_timer);
+    waitpid(child_pid, None).unwrap();
+    output
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-    
-    #[test]
-    fn simple_match() {
-        let command = String::from("echo hello world");
-        let pattern = String::from("hello world");
-        assert_eq!(monitor(command, pattern), String::from("hello world\n"));
+/// Runs the input command via fork + exec. Returns a buffered stream reader to the command's output and the PID
+/// of the spawn child process for the command.
+fn run_command(command: Vec<String>) -> (BufReader<File>, Pid) {
+    let (read_fd, write_fd) = pipe().expect("pipe failed"); //~10 micro for pipe
+    match unsafe { fork() } { //~500 micro for fork
+        Ok(ForkResult::Child) => {
+            //Redirect STDOUT to the write end of the pipe and close unnecessary fds
+            close(read_fd).unwrap();
+            unsafe { //Necessary here because nix::dup2 would require libc::STDOUT_FILENO to be an OwnedFd for extra safety, which we cannot do here without taking unnecessary steps
+                let dup_status = libc::dup2(write_fd.as_raw_fd(), libc::STDOUT_FILENO);
+                if dup_status == -1 { panic!("Redirecting STDOUT to the write end of the pipe failed: {}", std::io::Error::last_os_error()); }
+            }
+            close(write_fd).unwrap();
+            //Parse and execute command
+            //Note: Keep an eye on the .clones - perhaps there's a more efficient way to deal with them (dereferencing is not an option)?
+            let cmd = CString::new(command[0].clone()).unwrap();
+            let c_args: Vec<CString> = command.iter().map(|s| CString::new(s.clone()).unwrap()).collect();
+            execvp(&cmd, &c_args).expect("Command execution failed");
+            exit(1) //This line should never run, but Rust's type checker doesn't quite understand how execvp works 
+        }
+        Ok(ForkResult::Parent { child }) => {
+            close(write_fd).unwrap(); //The read_fd will be closed automatically when its owning object is dropped
+            // Return a BufReader on the read end of the pipe and the child process's PID
+            let pipe_file = std::fs::File::from(read_fd);
+            (BufReader::new(pipe_file), child)
+        }
+        Err(e) => panic!("Fork failed: {}", e)
     }
-    #[test]
-    #[should_panic]
-    fn simple_fail() {
-        let command = String::from("echo hello world");
-        let pattern = String::from("helo world");
-        monitor(command, pattern);
-    }
-    #[test]
-    fn basic_regex_match() {
-        let command = String::from("echo hello world.");
-        let pattern = String::from(r"[a-z]+ [a-z]*\.");
-        assert_eq!(monitor(command, pattern), String::from("hello world.\n"));
-    }
-    #[test]
-    #[should_panic]
-    fn basic_regex_fail() {
-        let command = String::from("echo hello w0rld.");
-        let pattern = String::from(r"[a-z]+ [a-z]*\.");
-        monitor(command, pattern);
-    }
-    #[test]
-    fn complex_regex_match() {
-        let command = String::from("ls -l");
-        let pattern = String::from(r"(total [0-9]+)|([drwxr-]+ +[0-9]+ +[^ ]+ +[^ ]+ +[0-9]+ +[^ ]+ +[0-9]+ +[a-zA-Z]+ +[0-9]+ + [0-9:]+ +.+)");
-        monitor(command, pattern); //Can't assert equivalence b/c variable output - not panicking should be good enough!
-    }
-    #[test]
-    #[should_panic]
-    fn complex_regex_fail() {
-        let command = String::from("ps -f");
-        let pattern = String::from(r"(UID( )+PID( )+PPID( )+C( )+STIME( )+TTY( )+TIME( )+CMD)|(([0-9a-zA-Z_]+|-)( )+[0-9]+( )+[0-9]+( )+[0-9]+( )+[0-9]+( )+[a-z0-9/?]+[^ ]+[0-9][0-9:]+( )+.+)");
-        monitor(command, pattern);
-    }
-    #[test]
-    fn complex_command() {
-        let command = String::from("ifconfig | grep 'inet ' | grep -v 127.0.0.1 | cut -f  2");
-        let pattern = String::from(r"^ *(~(inet +)|(inet +([0-9]+\.){3}[0-9]+))");
-        monitor(command, pattern);
-    }
-    #[test]
-    #[should_panic]
-    fn invalid_command() {
-        let command = String::from("not_a_command");
-        let pattern = String::from(r".*");
-        monitor(command, pattern);
-    }
-    #[test]
-    #[should_panic]
-    fn invalid_command_usage() {
-        let command = String::from("grep");
-        let pattern = String::from(r".*");
-        monitor(command, pattern);
-    }
+}
 
+/// Verifies that each line of the input buffered stream matches the input regex pattern, returning the full
+/// output if so and panicking if not.
+fn validate_stream(buffed_stream: BufReader<File>, pattern: String) -> String {
+    let pattern = Regex::new(&pattern).expect("Provided regular expression invalid");
+    //Line by line matching approach
+    let mut full_output = String::new();
+    for line_result in buffed_stream.lines() {
+        let line = line_result.unwrap();
+        //println!("Received line: {}", line);
+        if !pattern.is_match(&line) { panic!("Command output did not match pattern.\nPattern: {:?}\n Incriminating line of output: {:?}", pattern, line) }
+        full_output += line.as_str();
+    }
+    //Pass along full output if it is indeed valid
+    full_output
 }
